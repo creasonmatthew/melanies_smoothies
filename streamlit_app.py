f383f4bfbcfc1eddebcafaced0cd32943d3e4b36
creasonmatthew <creasonmatthew@lcsnet.com>
2025-06-03 10:39:28 -0500

    Update streamlit_app.py
    ordered code

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/:fruit_chosen")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -46,7 +48,6 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
 
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    #st.text(smoothiefroot_response.json())
     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
   
