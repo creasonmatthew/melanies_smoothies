da797c4783c5b42a54567020f1d4bffe9a8ff627
creasonmatthew <creasonmatthew@lcsnet.com>
2025-06-03 10:36:18 -0500

    Update streamlit_app.py
    formatted api display

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,6 @@ st.write(
   """
 )
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
@@ -47,7 +44,12 @@ if ingredients_list:
     # st.stop()
 
     time_to_insert = st.button('Submit Order')
-    
+
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    #st.text(smoothiefroot_response.json())
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+  
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
